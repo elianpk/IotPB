f19d30faf72cfa2b660b095b0d699e3a34dd1a2a
João Pedro <joao.araujo@totvs.com.br>
2022-03-28 23:11:38 -0300

    Solved issue with missed changes

--- Code/Operationalization/app/settings.py
+++ Code/Operationalization/app/settings.py
@@ -8,9 +8,9 @@ def get_enviroment_variables(name):
         env["ENDPOINT"] = None
 
     env["CLIENT_ID"] = "room"
-    env["PATH_TO_CERTIFICATE"] = "./certificates/room.cert.pem"
-    env["PATH_TO_PRIVATE_KEY"] = "./certificates/room.private.key"
-    env["PATH_TO_AMAZON_ROOT_CA_1"] = "./certificates/root-CA.crt"
+    env["PATH_TO_CERTIFICATE"] = f"./certificates/{name}/room.cert.pem"
+    env["PATH_TO_PRIVATE_KEY"] = f"./certificates/{name}/room.private.key"
+    env["PATH_TO_AMAZON_ROOT_CA_1"] = f"./certificates/{name}/root-CA.crt"
     env["TOPIC"] = "room/telemetry"
     
     return env
