be6dc8b9068c50c36d9e073ca411b9316b7d848b
EC2 Default User <ec2-user@ip-172-31-51-108.ec2.internal>
2022-04-07 22:53:32 +0000

    Created a object to send to AWS Analytics

--- Code/Operationalization/app/main.py
+++ Code/Operationalization/app/main.py
@@ -13,6 +13,11 @@ def csv():
     aws = Aws(get_enviroment_variables(name)) 
     for row in reader:
         print(row)
+        room = {
+            'Timestamp':row[0], 
+            'Temperature_Celsius':row[1],
+            'Relative_Humidity':row[2]
+        }
         aws.publish(row, 1)
     file.close()
     aws.disconnect()
