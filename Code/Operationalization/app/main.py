889e5809275e894fa86ade754b1a181d593c23b7
EC2 Default User <ec2-user@ip-172-31-51-108.ec2.internal>
2022-04-08 22:55:46 +0000

    Added shadow_control.js

--- Code/Operationalization/app/main.py
+++ Code/Operationalization/app/main.py
@@ -4,23 +4,38 @@ import time
 from settings import get_enviroment_variables
 from aws import Aws
 
-name = "elian"
+name = "shadow"
 
 def csv():
     import csv
     file = open("../../Data/Raw/room_temp_rawdata.csv")
     reader = csv.reader(file)
     aws = Aws(get_enviroment_variables(name)) 
+    next(reader)
     for row in reader:
         print(row)
         room = {
             'Timestamp':row[0], 
-            'Temperature_Celsius':row[1],
-            'Relative_Humidity':row[2]
+            'Temperature_Celsius': float(row[1]),
+            'Relative_Humidity':float(row[2])
         }
-        aws.publish(row, 1)
+        #aws.publish(room, 1)
+        air_conditioner_actuator(aws, float(row[1]))
     file.close()
     aws.disconnect()
+
+
+def air_conditioner_actuator(aws, temperature):
+    state = {
+          "state": {
+            "desired": {
+              "power": True # True if temperature >= 21 else False
+            }
+          }
+        }
+        
+    aws.publish(state, 1)
+
 
 def main():
     csv()
